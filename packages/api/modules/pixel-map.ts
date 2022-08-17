9fd3affb328ccb2f5e45985f311a8de4758ab4b3
Rida F'kih <hello@rida.dev>
2022-08-16 22:31:42 -0600

    limit pixel value to be between 0 and 255

--- packages/api/modules/pixel-map.ts
+++ packages/api/modules/pixel-map.ts
@@ -2,6 +2,7 @@ const pixels: number[] = [];
 
 export const flipPixels = (...flippedPixels: number[]) => {
   for (const pixel of flippedPixels) {
+    if (pixel < 0 || pixel >= 256) continue;
     const position = pixels.indexOf(pixel);
     if (position === -1) pixels.push(pixel);
     else pixels.splice(position, 1);
